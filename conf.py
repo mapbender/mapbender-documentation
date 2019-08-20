de6f0aee311b763547fb829e9d06b393fc19ac7c
pwheregroup <25791018+pwheregroup@users.noreply.github.com>
2019-08-20 13:52:31 +0200

    its 2019

--- conf.py
+++ conf.py
@@ -65,7 +65,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Mapbender'
-copyright = u'2018, The Mapbender Team'
+copyright = u'2019, The Mapbender Team'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
