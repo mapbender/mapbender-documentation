6d61f5ae63d7b82c4b7ad6d33122921676c0cc8b
pwheregroup <25791018+pwheregroup@users.noreply.github.com>
2019-08-20 13:59:50 +0200

    cause of technically rename of mb

--- conf.py
+++ conf.py
@@ -116,7 +116,7 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-#html_theme = 'mapbender3'
+#html_theme = 'mapbender'
 html_theme = "sphinx_rtd_theme"
 
 # Theme options are theme-specific and customize the look and feel of a theme
